dbe3df29f9256619289cbcf7ab2d9d1aae307181
udit22022000 <ping2udit@gmail.com>
2021-05-20 08:07:12 +0530

    added input template

---
+++ InputTemplate.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void file_i_o()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main(int argc, char const *argv[]) {
+	file_i_o();
+	//write your code here....
+
+
+
+	return 0;
+}
